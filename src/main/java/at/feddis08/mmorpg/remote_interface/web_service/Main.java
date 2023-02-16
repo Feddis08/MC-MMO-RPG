5e090b801ffb30e87b8e7328d1c8f512c52d0a66
feddis08 <feddis08@gmail.com>
2023-02-16 21:55:19 +0100

    ongoing - login/auth_token

--- src/main/java/at/feddis08/mmorpg/remote_interface/web_service/Main.java
+++ src/main/java/at/feddis08/mmorpg/remote_interface/web_service/Main.java
@@ -1,58 +1,64 @@
 package at.feddis08.mmorpg.remote_interface.web_service;
 
 import at.feddis08.mmorpg.MMORPG;
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import org.eclipse.jetty.server.session.DefaultSessionIdManager;
-import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.json.JSONObject;
 
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     private static Server server;
     public static Integer port = 8080;
 
     public static ArrayList<Web_user> web_users = new ArrayList<>();
+    public static Thread t = new Thread(){
+        @Override
+      public void run() {
+            int i = 0;
+            int remove_i = -1;
+            while (true){
+                Web_user web_user = web_users.get(i);
+                if (web_users.size() > i) {
+                    i = 0;
+                    if (remove_i != -1) {
+                        web_users.remove(remove_i);
+                        remove_i = -1;
+                    }
+                }
+                if (web_user.last_resp + web_user.timeout < System.currentTimeMillis())remove_i = i;
+                i++;
+            }
+        }
+    };
     public static void start() throws Exception {
         MMORPG.consoleLog("Starting WebService at port: " + port);
         server = new Server(port);
-
         ServletContextHandler handler = new ServletContextHandler();
-        SessionHandler sessionHandler = new SessionHandler();
-
-// Add the session handler to the server
-        server.setHandler(sessionHandler);
         handler.setContextPath("/");
         server.setHandler(handler);
-
-
-
-
-
-        HashSessionIdManager sessionIdManager = new HashSessionIdManager();
-        DefaultSessionIdManager defaultSessionIdManager = new DefaultSessionIdManager(server);
-        sessionIdManager.setWorkerName(defaultSessionIdManager.getWorkerName());
-        server.setSessionIdManager(sessionIdManager);
-
-        HashSessionManager sessionManager = new HashSessionManager();
-        SessionHandler sessionHandler = new SessionHandler(sessionManager);
-        server.setHandler(sessionHandler);
-
-
-
-
-
-
-
         handler.addServlet(ApiServlet.class, "/api");
 
         handler.addServlet(new ServletHolder(new HttpServlet() {
@@ -65,7 +71,7 @@ public class Main {
         }), "/*");
         server.start();
         server.join();
-
+        t.start();
         MMORPG.consoleLog("done...");
     }
 
--- src/main/java/at/feddis08/mmorpg/remote_interface/web_service/Parse_req.java
+++ src/main/java/at/feddis08/mmorpg/remote_interface/web_service/Parse_req.java
@@ -18,23 +18,6 @@ import java.util.Objects;
 public class Parse_req {
 
     public static String do_req(HttpServletRequest req) throws IOException, SQLException {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("ok", "true");
-        int i = 0;
-        Web_user web_user = null;
-        while (i < Main.web_users.size()){
-            if (Objects.equals(req.getSession().getAttribute("session_id"), Main.web_users.get(i).session_id)){
-                web_user = Main.web_users.get(i);
-            }
-            i ++;
-        }
-        if (web_user == null) {
-            String session_id = Methods.get_hash(String.valueOf(System.currentTimeMillis()));
-            web_user = new Web_user("", session_id);
-            req.getSession().setAttribute("session_id", session_id);
-            Main.web_users.add( web_user);
-        }
-
         StringBuilder buffer = new StringBuilder();
         BufferedReader reader = req.getReader();
         String response = "";
@@ -44,7 +27,29 @@ public class Parse_req {
         }
         String data = buffer.toString();
         if (!data.isBlank() || !data.isEmpty()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("ok", "true");
             JSONObject json_data = new JSONObject(data);
+            String session_id;
+            Web_user web_user = null;
+            try {
+                session_id = json_data.get("session_id").toString();
+                int i = 0;
+                while (i < Main.web_users.size()){
+                    if (Objects.equals(session_id, Main.web_users.get(i).session_id)){
+                        web_user = Main.web_users.get(i);
+                        web_user.last_resp = System.currentTimeMillis();
+                    }
+                    i ++;
+                }
+            }catch (Exception ignored){};
+            if (web_user == null) {
+                session_id = Methods.get_hash(String.valueOf(System.currentTimeMillis()));
+                web_user = new Web_user("", session_id, System.currentTimeMillis());
+                jsonObject.put("session_id", session_id);
+                Main.web_users.add( web_user);
+            }
+
             if (Objects.equals(json_data.get("req").toString(), "get server data")) {
                 RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
                 long uptime = rb.getUptime();
@@ -80,6 +85,30 @@ public class Parse_req {
                             //passed
                             jsonObject.put("need_set_up", "false");
                         }
+                    } else {
+                        jsonObject.put("status", "failed");
+                    }
+                }
+                response = jsonObject.toString();
+            }
+            if (Objects.equals(json_data.get("req").toString(), "login with token")) {
+                UserObject userObject = Functions.getUser("id", Functions.getPlayer("display_name", json_data.get("username").toString()).id);
+                if (Objects.equals(userObject.id, "")) {
+                    //user not found
+                    jsonObject.put("status", "failed");
+                } else {
+                    if (Objects.equals(userObject.jsonObject.get("auth_token").toString(), json_data.get("key").toString())) {
+                        web_user.id = userObject.id;
+
+                        userObject.jsonObject.put("logged_in", "true");
+                        userObject.jsonObject.put("auth_token", Methods.get_hash(String.valueOf(Math.random())));
+                        userObject.jsonObject.put("remote_host", req.getRemoteHost());
+                        userObject.jsonObject.put("last_login", String.valueOf(System.currentTimeMillis()));
+
+                        userObject.data_json = userObject.jsonObject.toString();
+                        Functions.update("users", "data_json", userObject.data_json, userObject.id, "id");
+                        jsonObject.put("status", "success");
+                        jsonObject.put("auth_token", userObject.jsonObject.get("auth_token"));
                     } else {
                         jsonObject.put("status", "failed");
                     }
