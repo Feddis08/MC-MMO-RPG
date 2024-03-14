4f6080c4e4c35c423943ad918048c4d2e783db0b
noawp1 <tobias.loeff123@gmail.com>
2024-03-14 07:40:32 +0100

    fixed code

--- src/main/java/at/feddis08/tools/discord/MessageListeners.java
+++ src/main/java/at/feddis08/tools/discord/MessageListeners.java
@@ -17,18 +17,18 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class MessageListeners {
-    public static void create_message_listener() {
+    public static void create_message_listener(){
         DISCORD.api.addMessageCreateListener(event -> {
-            if (!(String.valueOf(event.getMessage().getChannel().getId()).equals(DISCORD.config.server_log))) {
-                if (event.isPrivateMessage()) {
+            if (!(String.valueOf(event.getMessage().getChannel().getId()).equals(DISCORD.config.server_log))){
+                if (event.isPrivateMessage()){
                     Boot.consoleLog("DISCORD: got private_message: ; From: " + event.getMessage().getAuthor().getName() + "#" + event.getMessage().getAuthor().asUser().get().getMentionTag() + " ; Content: " + event.getMessage().getContent());
-                } else {
+                }else{
                     Boot.consoleLog("DISCORD: got server_message: ; Server: " + event.getServer().get().getName() + " ; Channel: " + event.getMessage().getChannel().asServerChannel().get().getName() + " ; From: " + event.getMessage().getAuthor().getName() + "#" + event.getMessage().getAuthor().asUser().get().getMentionTag() + " ; Content: " + event.getMessage().getContent());
                 }
             }
         });
         DISCORD.api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equals("!help")) {
+            if (event.getMessageContent().equals("!help")){
                 event.getChannel().sendMessage(
                         "```" +
                                 "commands: " +
@@ -43,22 +43,6 @@ public class MessageListeners {
             if (!(event.getMessage().getAuthor().getId() == DISCORD.api.getYourself().getId())) {
                 if (event.isPrivateMessage()) {
                     String[] message = event.getMessage().getContent().split(" ");
-                    if (Objects.equals(message[0], "!startup")) {
-                        if (message.length == 2) {
-                            try {
-                                Discord_playerObject dbDiscord_playerObject = Functions.getDiscordPlayer("discord_id", String.valueOf(event.getMessage().getAuthor().getId()));
-                                if (dbDiscord_playerObject.discord_id == null) {
-                                    dbDiscord_playerObject.discord_id = String.valueOf(event.getMessage().getAuthor().getId());
-                                    dbDiscord_playerObject.display_name = message[1];
-                                    Functions.createDiscordPlayer(dbDiscord_playerObject);
-                                } else {
-                                    event.getMessage().getAuthor().asUser().get().sendMessage("You already did the startup!");
-                                }
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    }
                     if (Objects.equals(message[0], "!startup")) {
                         if (message.length == 2) {
                             try {
@@ -121,9 +105,9 @@ public class MessageListeners {
                                                     "```"
                                     );
                                 }
-                                if (String.valueOf(event.getChannel().getId()).equals(DISCORD.config.chat)) {
-                                    if (!(event.getMessage().getAuthor().getId() == DISCORD.api.getYourself().getId())) {
-                                        Server.broadcast_chat_message("[" + dbRank.prefix + "][" + dbPlayer1.display_name + "]" + ": "
+                                if (String.valueOf(event.getChannel().getId()).equals(DISCORD.config.chat)){
+                                    if (!(event.getMessage().getAuthor().getId() == DISCORD.api.getYourself().getId())){
+                                        Server.broadcast_chat_message("[" +  dbRank.prefix +  "][" + dbPlayer1.display_name + "]" + ": "
                                                 + event.getMessage().getContent() + " [" + Methods.getTime() + "]");
                                         MMORPG.Server.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.BLUE + "DISCORD" + ChatColor.GRAY + "][" + ChatColor.GREEN + event.getMessage().getAuthor().getName() + ChatColor.GRAY + "]: " + ChatColor.YELLOW + event.getMessage().getContent() + ChatColor.GRAY + " [" + Methods.getTime() + "]");
                                     }
